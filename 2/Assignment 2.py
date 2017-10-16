04307b06d066c446f430e26e07e952eb17e9fde6
Descent098 <kieranw098@gmail.com>
2017-10-15 22:14:32 -0600

    Changed User_move back to String input for debugging purposes

--- 2/Assignment 2.py
+++ 2/Assignment 2.py
@@ -21,27 +21,27 @@ def user_move():
     """Function for defining what should happen during the players move"""
     op = 0
     while op < 1:
-        #old useless code unless you can prompt by string inputs
-        # dir = input('prompt')
-        # if dir == 'w'or 'W':
-        #     alex.fd(5)
-        #     op = 1
-        # if dir == 's' or 'S':
-        #     alex.fd(-5)
-        #     op = 1
-        # if dir == 'a' or 'A':
-        #     alex.lt(45)
-        #     op = 1
-        # if dir == 'd' or 'D':
-        #     alex.rt(45)
-        #     op = 1
+        #old useless code unless you can prompt by string inputs, just using for debugging
+        dir = input('prompt')
+        if dir == 'w'or 'W':
+            alex.fd(5)
+            op = 1
+        if dir == 's' or 'S':
+            alex.fd(-5)
+            op = 1
+        if dir == 'a' or 'A':
+            alex.lt(45)
+            op = 1
+        if dir == 'd' or 'D':
+            alex.rt(45)
+            op = 1
         
         #setting up the listner
-        qq.onkey(forward,"w")
-        qq.onkey(turn_left,"a")
-        qq.onkey(turn_right,"d")
-        qq.onkey(Backward,"s")
-        qq.listen()
+        #turtle.Screen().onkey(forward,"w")
+        #turtle.Screen().onkey(turn_left,"a")
+        #turtle.Screen().onkey(turn_right,"d")
+        #turtle.Screen().onkey(Backward,"s")
+        #turtle.Screen().listen()
     else:
             print('invalid input')
             op=0
