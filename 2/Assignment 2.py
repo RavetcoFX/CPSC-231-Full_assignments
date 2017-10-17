db36dacd64fd55f0582dbb0646dfa1805a90f195
descent098 <kieranw098@gmail.com>
2017-10-17 14:09:47 -0600

    did some stuff

--- 2/Assignment 2.py
+++ 2/Assignment 2.py
@@ -2,6 +2,9 @@ import tkinter as tk
 import turtle
 import random
 
+
+
+
 #need it to be 2/3rds of the time moving forward
 #random.randint(range)
 def forward():
@@ -20,24 +23,33 @@ def init_alex():
     """defining the turtle in here hopefully will make it so Take_turns can see alex and in turn user_move cant see the turtle"""
     alex = turtle.Turtle()
     return alex
+def init_alice():
+    """defining the turtle in here hopefully will make it so Take_turns can see alice and in turn alice_move cant see the turtle"""
+    alice = turtle.Turtle()
+    return alice
 def user_move(alex):
     """Function for defining what should happen during the players move"""
     op = 0
-    while op < 1:
+    while op == 0:
         #old useless code unless you can prompt by string inputs, just using for debugging
-        dir = input('prompt')
-        if dir == 'w'or 'W':
-            alex.fd(5)
+        dir = input('prompt:')
+        if dir == ('w') or ('W'):
+            alex.fd(30)
             op = 1
-        if dir == 's' or 'S':
-            alex.fd(-5)
-            op = 1
-        if dir == 'a' or 'A':
-            alex.lt(45)
-            op = 1
-        if dir == 'd' or 'D':
-            alex.rt(45)
-            op = 1
+            print('forward')
+            pass
+        #if dir == ('s') or ('S'):
+        #    alex.fd(-30)
+        #    op = 1
+        #    print('backward')
+        #if dir == ('a') or ('A'):
+        #    alex.lt(45)
+        #    op = 1
+        #    print('left')
+        #if dir == ('d') or ('D'):
+        #    alex.rt(45)
+        #    op = 1
+        #    print('right')
         
         #setting up the listner
         #turtle.Screen().onkey(forward,"w")
@@ -48,29 +60,37 @@ def user_move(alex):
     else:
             print('invalid input')
             op=0
+    return None
 
 def win_condition():
     """this will be the function that decides if the game is over"""
     return False
     #need an if statement for alice and alex within 30 pixels
 
-def Alice_move():
+def Alice_move(alice):
     """This will be the function that executes alices move after the player"""
+    numb = (random.uniform(1,6))//1
+    print(numb)
+    if numb == 1:
+        alice.lt(45)
+    if numb==2:
+        alice.rt(45)
+    else:
+        alice.fd(30)
     pass
 
-def Take_Turns(alex):
+def Take_Turns(alex,alice):
     """The function that calls each players turns to take place while the game hasn't been won"""
     while win_condition()==False:
         user_move(alex)
-        Alice_move()
+        Alice_move(alice)
 
 #random number generation
-random.uniform(0,1)
-print(random.uniform(1,2))
+
 
 def main():
     qq = turtle.Screen() #makes it so I don't have to type out turtle.screen()
-    Take_Turns(init_alex())
+    Take_Turns(init_alex(), init_alice())
     qq.mainloop()
 
 
