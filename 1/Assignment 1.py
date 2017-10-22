b78e92ce2923f070fdba7b93778ddfcdca8feeb8
Descent098 <kieranw098@gmail.com>
2017-10-22 14:59:22 -0600

    Fixed assignment 1 to the right version
    Fixed all of assignment 2's doc strings and comments

--- 1/Assignment 1.py
+++ 1/Assignment 1.py
@@ -1,37 +1,110 @@
-import turtle
+#Note that the program has to be fed a .dat file from the command line to run i.e C:\User>python3 < sample1.dat
 
-wm = turtle.Screen() #does some stuff
-wm.title('GUI Window') #sets GUI window name
-wm.bgcolor('steelblue') #sets background color
+import turtle #brings in digital etch-a-sketch Library; this is the GUI library that will draw the circle and lines, aswell as the intersects
+import math #brings in the math library; The program needs this to do the square root calcualtions
 
+qq = turtle.Screen() #makes it so I don't have to type out turtle.screen() constantly
+qq.title('Kieran Wood Assignment 1') #sets GUI window name
+
+#fixes the issue where the image is drawn off screen
+qq.setworldcoordinates(-200,-150,1000,800)
+
+#assigning values for all required variables to do the calculations from the .dat file:
+
+#These values define the coordinates for the center of the circle; The values are also used in the intersection calculations
+xc, yc = eval(input())
+
+#this is the value used for the radius of the circle being drawn; also used in the intersection calculations
+r = eval(input())
+
+#these are the variables used to draw the intersecting line and are also used in the intersection calculations
+x1, y1 = eval(input())
+x2,y2 = eval(input())
+
+
+#math to define the variables that will be used to give the intercepts
+a = ((x2-x1)**2)+((y2-y1)**2)
+b = (2*((x1-xc)*(x2-x1)+(y1-yc)*(y2-y1)))
+c = ((x1-xc)**2)+((y1-yc)**2)-(r**2)
+
+
+#defining all my functions
+def run_to_middle(cc):
+    """sets the position of jamie to the xc yc positions and moves it down one radius so that the circle is being drawn with xc, yc as the center from (0,0)"""
+    cc.up()
+    cc.setpos(xc,yc)
+    cc.lt(270)
+    cc.fd(r)
+    cc.lt(90)
+    cc.down()
+def Circlepoint(q):
+    """function for circling the location of the interceptsl; the q value is the return value from intersect_calc_pos() and,or intersect_calc_neg()"""
+    jamie.up()
+    if q==None:
+        pass
+    else:
+        jamie.setpos(q)
+        jamie.lt(-90)
+        jamie.fd(15)
+        jamie.lt(90)
+        jamie.down()
+        jamie.circle(15)
+        jamie.up()
+def drawline(bb):
+    bb.up()
+    bb.setpos(x1,y1)
+    bb.down()
+    bb.setpos(x2,y2)
+def intersect_calc_pos():
+    """Function to determine the intersection points of the line and the circle: this one does b Plus the square root of b squared minus 4ac all divided by 2a"""
+    if (((b**2)-4*a*c)) < 0:
+        return None
+        pass
+
+    if (((b**2)-4*a*c)) >= 0:
+        alpPos=((-b+math.sqrt(((b**2)-4*a*c)))/(2*a))
+        #print(alpPos)
+        if 1<alpPos or alpPos<0:
+            return None
+        else:
+            xpos = ((1-alpPos)*x1+(alpPos*x2))
+            ypos = ((1-alpPos)*y1+(alpPos*y2))
+            return (xpos,ypos)
+def intersect_calc_neg():
+    """Function to determine the intersection points of the line and the circle:this one does b minus the square root of b squared minus 4ac all divided by 2a"""
+    if (((b**2)-4*a*c)) < 0:
+        return None
+        pass
+
+    if (((b**2)-4*a*c)) >= 0:
+        alpNeg=((-b-math.sqrt(((b**2)-4*a*c)))/(2*a))
+        #print(alpNeg)
+        if 1<alpNeg or alpNeg<0:
+           return None
+        else:
+            yneg = ((1-alpNeg)*y1+(alpNeg*y2))
+            xneg = ((1-alpNeg)*x1+(alpNeg*x2))
+            return (xneg,yneg)
+
+#assigns a turtle the name jamie; it will execute all the functions 
 jamie = turtle.Turtle()
 
-#reads the first line of the file and assigns xc,yc
-xc,yc =  eval(input(1))
-r = eval(input(2))
-x1,y1 = eval(input(3))
+#speeds Jamie up so that it dosen't take so long to run each time
+jamie.speed(5)
 
+#uses the run to middle funciton; moves jamie to (xc,yx) and then moves jamie down r distance so that (xc,yc) is the center of the circle and not the bottom 
+run_to_middle(jamie)
 
+#draws a circle with a radius of r
+jamie.circle(r)
 
-#sets the position of the xc yc line from (0,0)
-jamie.setpos(xc,yc)
-# turtle.setpos(400,300)
+#calls the drawline function; draws a line from (x1,y1) to (x2,y2)
+drawline(jamie)
 
+#runs the circling function with whatever values return from the intersect calculation; The two functions correspond to subtracting and adding the b value before the square root respectively
+#if there are no intersections along the line it does nothing
+Circlepoint(intersect_calc_neg())
+Circlepoint(intersect_calc_pos())
 
-print(xc)
-print(yc)
-print(r)
-print(x1)
-print(y1)
-# sL = 60
-# basic rectangle
-# jamie.fd(sL)
-# jamie.lt(90)
-# jamie.fd(2*sL)
-# jamie.lt(90)
-# jamie.fd(sL)
-# jamie.lt(90)
-# jamie.fd(2*sL)
-# jamie.lt(90)
-
-wm.mainloop()
+#The mainloop that keeps the window open
+qq.mainloop()
--- 2/Assignment 2.py
+++ 2/Assignment 2.py
@@ -1,122 +1,130 @@
-import turtle
-import random
-import time
+import turtle #The library that will handle all the players, window and the majority of the games functionality
+import random #Library used to generate random numbers for the spawn positions of the players + deciding Alice's moves
+import time # Used to delay the end of the game so you can read the "You Win" 
 
 
 def init_jeff():
-    """Turtle that will handle writing text for the distance between alice and alice"""
+    """The function that initializes the turtle(jeff) that will handle writing text for the distance between alice and alice, as well as the turn/step number"""
     jeff = turtle.Turtle()
+    Top_left_corner = (-240,220) # the coordinate value for ~ the top left corner
     jeff.hideturtle()
     jeff.up()
-    jeff.setpos(-240,220)
+    jeff.setpos(Top_left_corner) #moving to a value close to the top left of the screen so as it does not get in the way while playing
     return jeff
     
 def init_alex():
     """defining the turtle in here hopefully will make it so Take_turns can see alex and in turn user_move cant see the turtle"""
     alex = turtle.Turtle()
+    #the values for shape, pen color, and fill color are based on the assignment requirements
     alex.shape("turtle")
     alex.pencolor('Blue')
     alex.fillcolor("Blue")
     return alex
+
 def init_alice():
     """defining the turtle in here hopefully will make it so Take_turns can see alice and in turn alice_move cant see the turtle"""
     alice = turtle.Turtle()
+    #the values for shape, pen color, and fill color are based on the assignment requirements
     alice.shape("turtle")
     alice.pencolor("Red")
     alice.fillcolor("Red")
     return alice
+
 def use_turtles(alex,alice):
-    """A function that allows you to call each turtle without recreating the turtle each time"""
+    """A function that allows you to call each turtle without recreating the turtle each time; needs both alex and alice as arguments"""
     alex = alex
     alice = alice
     return [alex,alice]
     
-def startpoint(turtles, window): #calling window_height_width just window for sanity
-    """Function that carries out the movement of each turtle to a random location in the canvas at the start of each game"""
-    alex = turtles[0]
-    alice = turtles[1]
-    max_limit = ((window//2) - 10) # this was it won't land exactly on the edge or outside the window
+def startpoint(turtles, window_height_width):
+    """Function that carries out the movement of each turtle to a random location in the canvas at the start of each game; needs both turtles as a list + the max value of the window for arguments"""
+    alex = turtles[0] #strips alex from the list of [alex, alice] in use_turtles()
+    alice = turtles[1] #strips alice from the list of [alex, alice] in use_turtles()
+    max_limit = ((window_height_width//2) - 10) #Uses the windows maximum height and width value -10 so that the turtles cannot spawn outside the screen
     
-    alex.up()
-    alex.setpos((random.randint(0,max_limit)),(random.randint(0,max_limit)))
+    alex.up()#so that when alex gets moved a line isnt drawn from (0,0) to the location
+    alex.setpos((random.randint(0,max_limit)),(random.randint(0,max_limit))) #after spawning alex, move to a random location within the canvas; this will be the starting position for alex
     alex.down()
     
-    alice.up()
-    alice.setpos((random.randint(0,max_limit)),(random.randint(0,max_limit)))
+    alice.up() #so that when alice gets moved a line isnt drawn from (0,0) to the location
+    alice.setpos((random.randint(0,max_limit)),(random.randint(0,max_limit)))#after spawning alice, move to a random location within the canvas; this will be the starting position for alice
     alice.down()
     return turtles
 
 
 def user_move(alex, alice):
-    """Function for defining what should happen during the players (alex's) move"""
-    op = 0
+    """Function for defining what should happen during the players (alex's) move; needs both alex and alice as an input argument"""
+    Alex_Forward_by = 30 # The amount of pixels alex should move forward by
+    Alex_Turn_by = 45 #The angle, in degrees that Alex should turn in a direction by
+    op = 0 #arbitrary variable to handle invalid inputs
     while op == 0:
         direction = input('prompt:')
         if (('w') or ('W')) is direction:
-            alex.fd(30)
+            alex.fd(Alex_Forward_by)
             op = 1
             print('forward')
-        if (('s') or ('S')) is direction:
-            alex.fd(-30)
-            op = 1
-            print('backward')
         if (('a') or ('A')) is direction:
-            alex.lt(45)
+            alex.lt(Alex_Turn_by)
             op = 1
             print('left')
         if (('d') or ('D')) is direction:
-            alex.rt(45)
+            alex.rt(Alex_Turn_by)
             op = 1
             print('right')
-        if ('cheat') in direction:
-            alex.setpos(alice.pos())
-            print('cheater')
-            op = 1
+        #Code for automatically winning the round; used to test win condition
+       # if ('cheat') in direction:
+        #    alex.setpos(alice.pos())
+         #   print('cheater')
+          #  op = 1
         if op == 0:
             op=0
             print('invalid input')
-        if op == 2:
-            pass
 
-def turtle_looparound(name):
-
-    if name.xcor() <= -250:
+def Fix_Offscreen(name):
+    """Function for handling (in a way similar to pacman) if the turtles move out of the canvas (off screen); the only argument passed is the name of the turtle"""
+    Negative_Max = -250 #The lowest possible x or y value that is within the canvas
+    Negative_Max_New = 240 #The coordinate to relocate the turtle to if they go past Negatice_Max value in the x or y
+    Positive_Max = 250 #The Highest possible x or y value that is within the canvas
+    Positive_Max_new = -240 ##The coordinate to relocate the turtle to if they go past the Positive_Max value in the x or y
+    if name.xcor() <= Negative_Max:
         name.up()
         name.ht()
-        name.setx(240)
+        name.setx(Negative_Max_New)
         name.down()
         name.st()
-    if name.xcor() >= 250:
+    if name.xcor() >= Positive_Max:
         name.up()
         name.ht()
-        name.setx(-240)
+        name.setx(Positive_Max_new)
         name.down()
         name.st()
-    if name.ycor() >= 250:
+    if name.ycor() >= Positive_Max:
         name.up()
         name.ht()
-        name.sety(-240)
+        name.sety(Positive_Max_new)
         name.down()
         name.st()
-    if name.ycor() <= -250:
+    if name.ycor() <= Negative_Max:
         name.up()
         name.ht()
-        name.sety(240)
+        name.sety(Negative_Max_New)
         name.down()
         name.st()
 
 
 def Alice_move(alice, alex):
     """This will be the function that executes alices move after the player"""
-    numb = (random.uniform(1,6))//1
+    Alice_Forward_by = 20 # The amount of pixels alice should move forward by
+    Alice_Turn_by = 90 #The angle, in degrees that Alice should turn in a direction by
+    numb = (random.uniform(1,6))//1) #Does the random number generation that determines Alice's turn
     if alex.pos() == alice.pos():
         pass
     if numb == 1:
-        alice.lt(45)
+        alice.lt(Alice_Turn_by)
     if numb==2:
-        alice.rt(45)
+        alice.rt(Alice_Turn_by)
     else:
-        alice.fd(20)
+        alice.fd(Alice_Forward_by)
     pass
     
     
@@ -128,29 +136,29 @@ def Game_loop(turtles):
     win = False
     jeff = init_jeff()
     each_turn_count = 0
-    while not win:
-        each_turn_count += 1
-        jeff.write(('Step #%i - The Distance: %i' % (each_turn_count, ((alex.distance(alice,alice))) // 1)), False, align="left")
-        user_move(alex, alice)
-        turtle_looparound(alex)
-        Alice_move(alice, alex)
-        turtle_looparound(alice)
-        jeff.clear()
-        jeff.write(('The Distance: ', ((alex.distance(alice,alice)) // 1)), False, align="left")
-        jeff.clear()
-        if ((alex.distance(alice,alice)) // 1) < 30 and alex.isdown() and alice.isdown():
+    while not win: #While the game still hasn't been won each player takes their turn, and jeff presents the games information until the game is won
+        each_turn_count += 1 #The value for which turn the game is on; used by jeff to present this information to the player
+        jeff.write(('Step #%i - The distance between Alex and Alice is: %i' % (each_turn_count, ((alex.distance(alice,alice))) // 1)), False, align="left")
+        user_move(alex, alice) #Alex takes his turn
+        Fix_Offscreen(alex) #checks to make sure Alex is not outside the boundary; if he is then fix it
+        Alice_move(alice, alex) #Alice takes her turn
+        Fix_Offscreen(alice)#checks to make sure Alice is not outside the boundary; if he is then fix it
+        jeff.clear() #after each turn the text has to be cleares so that it does not overlap with the last turn
+        if ((alex.distance(alice,alice)) // 1) < 30 and alex.isdown() and alice.isdown(): # if you win jeff writes you win, the title of the window changes and after 3 seconds the screen clears and the game restarts
             win = True
             jeff.clear()
             jeff.write("You Win")
+            turtle.Screen().title("You Win - Game will restart shortly")
             time.sleep(3)
             turtle.clearscreen()
             main()
     
 
 def main():
-    window_height_width = 500
-    turtle.Screen().setup(window_height_width,window_height_width)
-    Game_loop(startpoint(use_turtles(init_alex(), init_alice()), window_height_width))
+    window_height_width = 500 #This is the maximum Height and Width of the window 
+    turtle.Screen().setup(window_height_width,window_height_width) #uses the window_height_width value to create a square aspect ratio window (i.e. 500x500)
+    turtle.Screen().title("Assignment 2 - Chasing Alice - Kieran Wood") #This sets the Turtle window to the right name
+    Game_loop(startpoint(use_turtles(init_alex(), init_alice()), window_height_width)) #This calls all the functions in the right order to play the game; initializes the turtles and passes them as arguments to use_turtles, which is then brought into startpoint and then brought into the main game loop
     
     
 main()
