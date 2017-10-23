d144bfc708cb766abe7dff89a9f260f6af434d57
Descent098 <kieranw098@gmail.com>
2017-10-22 23:47:56 -0600

    Final commit of assignment 2

--- 2/Assignment 2.py
+++ 2/Assignment 2.py
@@ -43,7 +43,7 @@ def startpoint(turtles, window_height_width):
     max_limit = ((window_height_width//2) - 10) #Uses the windows maximum height and width value -10 so that the turtles cannot spawn outside the screen
     
     alex.up()#so that when alex gets moved a line isnt drawn from (0,0) to the location
-    alex.setpos((random.randint(0,max_limit)),(random.randint(0,max_limit))) #after spawning alex, move to a random location within the canvas; this will be the starting position for alex
+    alex.setpos(0,0) #after spawning alex, move to the center of the  canvas; this will be the starting position for alex
     alex.down()
     
     alice.up() #so that when alice gets moved a line isnt drawn from (0,0) to the location
@@ -70,7 +70,11 @@ def user_move(alex, alice):
         if (('d') or ('D')) is direction:
             alex.rt(Alex_Turn_by)
             is_invalid_input = 1
-            
+        
+        if (('s') or ('S')) is direction:
+            alex.fd(-1*Alex_Turn_by)
+            is_invalid_input = 1
+
         #Code for automatically winning the round; used to test win condition
        # if ('cheat') in direction:
         #    alex.setpos(alice.pos())
@@ -78,14 +82,15 @@ def user_move(alex, alice):
           #  is_invalid_input = 1
         if is_invalid_input == 0:
             is_invalid_input=0
-            print('invalid input please input one of these values \nw= forward \na= left turn \nd = right turn')
+            print('invalid input please input one of these values \nw = forward \na = left turn \nd = right turn')
 
-def Fix_Offscreen(name):
-    """Function for handling (in a way similar to pacman) if the turtles move out of the canvas (off screen); the only argument passed is the name of the turtle"""
+def Fix_Offscreen(name,window_height_width):
+    """Function for handling if the turtles move out of the canvas (off screen); the only argument passed is the name of the turtle"""
+    max_limit = ((window_height_width//2) - 10) #Uses the windows maximum height and width value -10 so that the turtles cannot move outside the screen
     Negative_Max = -250 #The lowest possible x or y value that is within the canvas
-    Negative_Max_New = 240 #The coordinate to relocate the turtle to if they go past Negatice_Max value in the x or y
+    Negative_Max_New = (random.randint(0,max_limit)) #The coordinate to relocate the turtle to if they go past Negatice_Max value in the x or y
     Positive_Max = 250 #The Highest possible x or y value that is within the canvas
-    Positive_Max_new = -240 ##The coordinate to relocate the turtle to if they go past the Positive_Max value in the x or y
+    Positive_Max_new = (random.randint(0,max_limit)) ##The coordinate to relocate the turtle to if they go past the Positive_Max value in the x or y
     if name.xcor() <= Negative_Max:
         name.up()
         name.ht()
@@ -127,16 +132,16 @@ def Alice_move(alice, alex):
         alice.fd(Alice_Forward_by)
     pass
 
-def win_condition(alex, alice, jeff, each_turn_count):
+def win_condition(alex, alice, jeff, each_turn_count,window_height_width):
     """Handles determining if the game is won or not; if the game is won it will restart the game, if not it will let each turtle take their turn"""
     win = False #Initially set win to False as long as alex and alice did not spawn within 30 pixels
     while not win: #While the game still hasn't been won each player takes their turn, and jeff presents the games information until the game is won
         each_turn_count += 1 #The value for which turn the game is on; used by jeff to present this information to the player
         jeff.write(('Step #%i - The distance between Alex and Alice is: %i' % (each_turn_count, ((alex.distance(alice,alice))) // 1)), False, align="left") #Jeff will write the turn/step# + the distance between the turtles each turn
         user_move(alex, alice) #Alex takes his turn
-        Fix_Offscreen(alex) #checks to make sure Alex is not outside the boundary; if he is then fix it
+        Fix_Offscreen(alex, window_height_width) #checks to make sure Alex is not outside the boundary; if he is then fix it
         Alice_move(alice, alex) #Alice takes her turn
-        Fix_Offscreen(alice)#checks to make sure Alice is not outside the boundary; if he is then fix it
+        Fix_Offscreen(alice,window_height_width)#checks to make sure Alice is not outside the boundary; if he is then fix it
         jeff.clear() #after each turn the text has to be cleares so that it does not overlap with the last turn
         if ((alex.distance(alice,alice)) // 1) < 30 and alex.isdown() and alice.isdown(): # if you win jeff writes you win, the title of the window changes and after 3 seconds the screen clears and the game restarts
             win = True
@@ -147,20 +152,20 @@ def win_condition(alex, alice, jeff, each_turn_count):
             turtle.clearscreen()
             main()
 
-def Game_loop(turtles):
+def Game_loop(turtles,window_height_width):
     """The function that brings in all the turtles after their setup stages, sets the turn count to 0, then calls the win_condition funciton"""
     alex= turtles[0]
     alice= turtles[1]
     jeff = init_jeff()
     each_turn_count = 0
-    win_condition(alex, alice, jeff, each_turn_count)
+    win_condition(alex, alice, jeff, each_turn_count,window_height_width)
     
 
 def main():
     window_height_width = 500 #This is the maximum Height and Width of the window 
     turtle.Screen().setup(window_height_width,window_height_width) #uses the window_height_width value to create a square aspect ratio window (i.e. 500x500)
     turtle.Screen().title("Assignment 2 - Chasing Alice - Kieran Wood") #This sets the Turtle window to the right name
-    Game_loop(startpoint(use_turtles(init_alex(), init_alice()), window_height_width)) #This calls all the functions in the right order to play the game; initializes the turtles and passes them as arguments to use_turtles, which is then brought into startpoint and then brought into the main game loop
+    Game_loop(startpoint(use_turtles(init_alex(), init_alice()), window_height_width),window_height_width) #This calls all the functions in the right order to play the game; initializes the turtles and passes them as arguments to use_turtles, which is then brought into startpoint and then brought into the main game loop
     
     
 main()
