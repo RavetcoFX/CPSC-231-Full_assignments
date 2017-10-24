8e7aa91cf3ad1dd2318e241959595b348cde60aa
Descent098 <kieranw098@gmail.com>
2017-10-23 23:17:57 -0600

    Initial Commit; Started framework building

---
+++ 3/Assignment 3.py
@@ -0,0 +1,38 @@
+
+def create_window():
+    """Function that will create the window the game will be played in"""
+    pass
+
+
+def Set_Up_Initial_Variables():
+    Amount_of_Nuts = (eval(input("How many nuts are there on the table initially (10-100)?")))
+    print((('%i Nuts Selected, The game will begin counting down from: %i nuts' % (Amount_of_Nuts,Amount_of_Nuts)
+
+
+
+
+
+
+def How_many_players():
+    """function that will give you an input option to choose if you want to play with one or two players"""
+    Players = eval(input("Options: \nPlay against a friend (1) \nPlay against a computer (2) \nWhich Option do you take (1-2)?:"))
+    slection = 0
+    while selection == 0:
+        if Players ==1:
+            print("You have selected a two player game")
+            selection =1
+        if Players ==2:
+            print("You have selected to play against an AI")
+            selection =1
+        else:
+            print("please select a valid option")
+            selection = 0 
+
+def Main():
+    """The main function that will get called to play the game"""
+    print("Welcome to the game of nuts!")
+    Set_Up_Initial_Variables()
+    How_many_players()
+    pass
+
+Main()
