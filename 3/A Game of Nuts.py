b26aaa77b55f0affc133cc83a5924f2566956f87
Descent098 <kieranw098@gmail.com>
2017-10-29 00:27:07 -0600

    Fixed Win condition state on player vs player

--- 3/A Game of Nuts.py
+++ 3/A Game of Nuts.py
@@ -22,7 +22,8 @@ def How_Many_Players(Amount_of_Nuts):
     """function that will give you an input option to choose if you want to play with one or two players"""
     Players = eval(input("Options: \nPlay against a friend (1) \nPlay against an untrained computer (2) \nPlay against a trained AI (3) \nWhich Option do you take (1-3)?:"))
     selection = 0
-    while selection == 0:
+    Win = endgame_repeat(Amount_of_Nuts) # Because this is the outermost function being called in the Main function it needs to know if it should terminate
+    while ((selection == 0) and (Win == False)):
         if Players == 1:
             print("You have selected a two player game")
             selection =1
@@ -39,13 +40,14 @@ def How_Many_Players(Amount_of_Nuts):
         else:
             print("please select a valid option")
             selection = 0 
+    pass
            
     
 
 def Player_vs_Player(Amount_of_Nuts,Players):
     Win = endgame_repeat(Amount_of_Nuts)
     if ((Players == 1) and (Win ==False)):
-        
+        Win = endgame_repeat(Amount_of_Nuts)
         while Win == False:
             Player_one_Turn = (eval(input("Player one; How many Nuts do you take (1-3)?:")))
             if ((Player_one_Turn >=1) and (Player_one_Turn <= 3)):
@@ -55,18 +57,20 @@ def Player_vs_Player(Amount_of_Nuts,Players):
                     Player_one_Turn = (eval(input("Invalid selection, try again \nPlayer one; How many Nuts do you take (1-3)?:")))
 
             print("Amount of nuts remaining %d" %(Amount_of_Nuts))
-            endgame_repeat(Amount_of_Nuts)
-            
-            Player_two_Turn = (eval(input("Player two; How many Nuts do you take (1-3)?:")))
-            if ((Player_two_Turn >=1) and (Player_two_Turn <= 3)):
-               Amount_of_Nuts = Amount_of_Nuts - Player_two_Turn
-            if not ((Player_two_Turn >=1) and (Player_two_Turn <= 3)):
-                while (Player_two_Turn < 1) or (Player_two_Turn > 3):
-                    Player_two_Turn = (eval(input("Invalid selection, try again \nPlayer one; How many Nuts do you take (1-3)?:")))
+            print(Win)
+
+            Win = endgame_repeat(Amount_of_Nuts)
+            if Win == False: #Player one will be invalidated because of the while loop but in between the turns, the win variable dosen't get checked
+                Player_two_Turn = (eval(input("Player two; How many Nuts do you take (1-3)?:")))
+                if ((Player_two_Turn >=1) and (Player_two_Turn <= 3)):
+                   Amount_of_Nuts = Amount_of_Nuts - Player_two_Turn
+                if not ((Player_two_Turn >=1) and (Player_two_Turn <= 3)):
+                    while (Player_two_Turn < 1) or (Player_two_Turn > 3):
+                        Player_two_Turn = (eval(input("Invalid selection, try again \nPlayer one; How many Nuts do you take (1-3)?:")))
+                print(Win)
 
 
-            print("Amount of nuts remaining %d" %(Amount_of_Nuts))
-            endgame_repeat(Amount_of_Nuts)
+                print("Amount of nuts remaining %d" %(Amount_of_Nuts))
             
     else:
         pass 
@@ -80,14 +84,14 @@ def initHats(amountofnuts):
 
 
 def endgame_repeat(Amount_of_Nuts):
-    if Amount_of_Nuts <= 0:
+    if (Amount_of_Nuts <= 0):
         Win = True
         repeat = eval(input("Would You Like to play again? \n1 for Yes 2, for No \nI choose:"))
         if repeat == 1:
             Main()
         if repeat == 2:
             print('Thanks For playing')
-            pass
+            return Win
 
         else:
             repeat = eval(input("Invalid Option Selected Please Choose one of the Below options: \nWould You Like to play again? \n1 for Yes 2, for No \nI choose:"))
